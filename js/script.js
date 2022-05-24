b6bf4d5a079d63f836d4cc89a8da196eb99bace7
jk <jk92lania@gmail.com>
2022-05-24 12:55:30 +0900

    mobile 메뉴 추가, visual 추가

--- js/script.js
+++ js/script.js
@@ -19,17 +19,66 @@ $(document).ready(function () {
 
     // 스크롤 기능
     let header = $('.header')
-    $(window).scroll(function(){
-        let temp = $(window).scrollTop();
-        if(temp > 0) {
-            header.addClass('fix-active');
-        } else {
-            header.removeClass('fix-active');
+    function fixHeader(){
+        $(window).scroll(function(){
+            let temp = $(window).scrollTop();
+            if(temp > 0) {
+                header.addClass('fix-active');
+            } else {
+                header.removeClass('fix-active');
+    
+            }
+        });
+    }
 
+    // 모바일 메뉴
+    let mBt = $('.m-bt');
+    let mMenuBg = $('.m-menu-bg');
+    let mMuenuWrap = $('.m-menu-wrap');
+    let mBtClose = $('.m-bt-close');
+
+    mBt.click(function(){
+        mMenuBg.addClass('m-menu-active');
+        mMuenuWrap.addClass('m-menu-active');
+        
+    });
+    mBtClose.click(function(){
+        mMenuBg.removeClass('m-menu-active');
+        mMuenuWrap.removeClass('m-menu-active');
+        
+    });
+    mMenuBg.click(function(){
+        mMenuBg.removeClass('m-menu-active');
+        mMuenuWrap.removeClass('m-menu-active');
+        
+    });
+
+    
+    
+    // 화면 리사이징 처리
+    $(window).resize(function(){
+        let w = $(window).width();
+        if(w > 700) {
+            mMenuBg.removeClass('m-menu-active');
+            mMuenuWrap.removeClass('m-menu-active');
+            fixHeader();
         }
     });
 });
 
-window.onload = function(){
 
+window.onload = function(){
+    // visual slide
+    let sw_visual = new Swiper('.sw-visual', {
+        loop : true,
+        autoplay : {
+            delay: 2000,
+            disableOnInteraction: false,
+          },
+        navigation : {
+            prevEl : ".sw-visual-prev",
+            nextEl: ".sw-visual-next",
+        }
+        
+    });
 }
